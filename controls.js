5414163531656cdb240658cb7236db0090971a65
Donald Highe <dhighe25@gmail.com>
2016-10-07 17:10:42 -0400

    Worked on collision, working on ability to jump off blocks

--- controls.js
+++ controls.js
@@ -1,101 +1,114 @@
-//Numbers are not finalized, basic player movement for cardinal directions
-$(document).ready(function (){
-    $("body").keydown(function(event){
-      if(event.keyCode === 38){
-        $(".player2").animate({top: "-=10px"},{duration: 2});
-      } else if (event.keyCode === 39){
-        $(".player2").animate({left: "+=10px"},{duration: 2});
-      } else if(event.keyCode === 37){
-        $(".player2").animate({left: "-=10px"},{duration: 2});
-      } else if(event.keyCode === 40){
-        $(".player2").animate({top: "+=10px"},{duration: 2});
-        // A shooter has to be able to fire some bullets
-      } else if(event.keyCode === 87){
-        console.log('Fire the cannon!');
-        fire();
-      } else if(event.keyCode === 83){
-        console.log('Fire the other cannon!');
-        fire2();
-      } else if(event.keyCode === 68){
-        console.log('Fire the right cannon!');
-        fire3();
-      } else if(event.keyCode === 65){
-        console.log('Fire the left cannon!');
-        fire4();
+//Please let everything be fine
+console.log('Everything is fine.')
+
+//Collision detection between player and block
+function hitbox(){
+  var playerLeft = $('.player').offset().left;
+  var playerRight = $('.player').offset().left + $('.player').width();
+  var playerTop = $('.player').offset().top;
+  var playerBottom = $('.player').offset().top + $('.player').height();
+  var blockLeft = $('.block').offset().left;
+  var blockRight = $('.block').offset().left + $('.block').width();
+  var blockTop = $('.block').offset().top;
+  var blockTopS = $('.block').offset().top + 5;
+  var blockTopN = $('.block').offset().top - 5;
+  var blockBottom = $('.block').offset().top + $('.block').height();
+
+//Killzone are on blocks side but safe on top
+  if (playerRight > blockLeft && playerLeft < blockRight && playerBottom > blockTop){
+    console.log('Hit!');
+  } else if ( playerRight > blockLeft && playerLeft < blockRight && blockTopS > playerBottom && playerBottom > blockTopN){
+    console.log('Land!');
+    $('.player').animate({top: '-50'});
+  }
+}
+//Collision with triangles which are deadly from all sides.. true story
+function hitHazzard(){
+  $('.triangle').each(function(){
+    var playerLeft = $('.player').offset().left;
+    var playerRight = $('.player').offset().left + $('.player').width();
+    var playerTop = $('.player').offset().top - $('.player').height();
+    var playerBottom = $('.player').offset().top;
+    var triangleLeft = $(this).offset().left;
+    var triangleRight = $(this).offset().left + 50;
+    var triangleTop = $(this).offset().top - 50;
+    var triangleBottom = $(this).offset().top;
+
+    if (playerRight > triangleLeft  && playerLeft < triangleRight && playerBottom > triangleTop){
+      console.log('Hit!');
+    }
+  });
+}
+
+var inAir = false
+
+$(document).ready(function jumping(){
+  $('body').keydown(function(event) {
+    if (event.keyCode === 32 && inAir === false) {
+      $('.player').animate({top: '-=100'},{duration: 450});
       }
-    });
+    inAir = true;
+  });
+  $('body').keyup(function(event) {
+    if(event.keyCode === 32 && inAir === true) {
+      $('.player').animate({top: '+=100'},{duration: 450});
+    }
+    if ($('.player').offset().top != 416){
+      $('.player').offset().top = 416;
+    }
+    inAir = false;
+  });
 });
 
 
-//Again Numbers are not finalized, testing projectiles
-function fire() {
-  var cannonHead = $('.cannon').position().top;
-  $(".cannon").append($("<div>").addClass("fire").css({"top": cannonHead}));
-}
+// function platform() {
+//     $(".block").each(function() {
+//       var movright =  $(".block").position().left;
+//       var boundaryLeft = -3000;
+//       $('.block').animate({left: '-=4000'},{duration: 8000});
+//       hitbox();
+//       if (movright < boundaryLeft){
+//         $(this).remove();
+//     }
+//   });
+// }
 
-function fire2() {
-  var cannonHead = $('.cannon').position().top;
-  $(".cannon").append($("<div>").addClass("fire2").css({"top": cannonHead}));
-}
+// function platform2() {
+//     $(".triangle").each(function() {
+//       var movright =  $(".triangle").position().left;
+//       var boundaryLeft = -3000;
+//       $('.triangle').animate({left: '-=4000'},{duration: 8000});
+//       hitbox();
+//       if (movright < boundaryLeft){
+//         $(this).remove();
+//     }
+//   });
+// }
 
-function fire3() {
-  var cannonHead = $('.cannon').position().top;
-  $(".cannon").append($("<div>").addClass("fire3").css({"left": cannonHead}));
-}
+// setInterval(platform, 1000);
+// setInterval(platform2, 1000);
+setInterval(hitbox, 10);
+setInterval(hitHazzard, 100);
 
-function fire4() {
-  var cannonHead = $('.cannon').position().top;
-  $(".cannon").append($("<div>").addClass("fire4").css({"left": cannonHead}));
-}
 
-function update() {
-    $(".fire").each(function() {
-      var oldTop = $(this).offset().top;
-      var boundaryCeiling = $('body').position().top;
-      $(this).css("top", oldTop - 10);
-      if (oldTop < boundaryCeiling){
-        $(this).remove();
-    }
-  });
-}
+//Automatic motion for the player object, for the person playing the game, they will just have to dodge other objects on screen
 
-function update2() {
-    $(".fire2").each(function() {
-      var oldTop = $(this).offset().top;
-      var boundaryFloor = $('body').position().top + $('body').height();
-      $(this).css("top", oldTop + 10);
-      if (oldTop > boundaryFloor){
-        $(this).remove();
-    }
-  });
-}
+//Gravity Function
 
-function update3() {
-    $(".fire3").each(function() {
-      var oldLeft = $(this).offset().left;
-      var boundaryRight = $('body').position().left + $('body').width();
-      $(this).css("left", oldLeft + 10);
-      if (oldLeft > boundaryRight){
-        $(this).remove();
-    }
-  });
-}
 
-function update4() {
-    $(".fire4").each(function() {
-      var oldLeft = $(this).offset().left;
-      var boundaryLeft = $('body').position().left;
-      $(this).css("left", oldLeft - 10);
-      if (oldLeft < boundaryLeft){
-        $(this).remove();
-    }
-  });
-}
 
-setInterval(update, 10);
-setInterval(update2, 10);
-setInterval(update3, 10);
-setInterval(update4, 10);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
